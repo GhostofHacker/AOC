74d590ad38f51343132e1bad6310305a2907a8d1
GhostofHacker <ilyasghost3@gmail.com>
2024-12-02 17:54:38 +0300

    2024 Day 1 Puzzle

--- 2023_day_2.py
+++ 2023_day_2.py
@@ -1,1 +1,13 @@
-##...
+##Whe landed in snow island
+##An elf gives us a small game to spend time in mean time??
+##In a bag there are blue, red and green cubes inside of it. 
+##Elf randomly pulls out cubes each round of each game.
+## Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+## Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+## Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+##Elf askes which games would be possible if bag had only contained 12 red, 13 green, 14 blue cubes?
+##Game 1 and 2 would be possible if you look at 3 game there once showed 20 red thus making it impossible!
+##Then you add ID od each game to a sum in this case is 3!
+
+##Now tell the sum of ID's where 12 red, 13 green, 14 blue cubes is possible?
+
---
+++ 2024_day_1.py
@@ -0,0 +1,4 @@
+##There's two lists containing the locations of places. Its unique name is location IDs
+##Pair the left and right smallest numbers with each other. Then find the distance between them.
+##Then add all the distance in to one to figure out how far all is!
+## 3 - 4 = 1; 4 - 7 = 3; || 1 + 3 = 4! 4 is the total of the distance!
