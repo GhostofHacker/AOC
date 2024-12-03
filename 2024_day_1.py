3c257d652a5e201651f2cda93d580f5b6fc5f1e8
GhostofHacker <ilyasghost3@gmail.com>
2024-12-03 17:12:45 +0300

    Solved Day 1 of 2024, added Day 2 of 2024

--- 2024_day_1.py
+++ 2024_day_1.py
@@ -4,10 +4,12 @@
 ## 3 - 4 = 1; 4 - 7 = 3; || 1 + 3 = 4! 4 is the total of the distance!
 
 cal_doc = open('2024_day_1_input.txt', 'r')
+list_1 = []; list_2 = []
+totalNum = 0; totalDistance = 0; similarity_score = 0;
 
-def part_one():
+def sortedList():
     cordinateID_1 = ''; cordinateID_2 = ''
-    list_1 = []; list_2 = []
+    global totalNum
 
     for line in cal_doc:
         for i in range(0, 5):
@@ -20,6 +22,45 @@ def part_one():
 
         cordinateID_1 = ''; cordinateID_2 = ''
 
-    print(list_2)
+        totalNum = totalNum + 1
+    
+    list_1.sort()
+    list_2.sort()
 
-part_one()
+def part_one():
+    sortedList()
+    global totalDistance
+
+    for x in range(0, totalNum):
+        if(list_1[x] - list_2[x] < 0):
+            totalDistance = totalDistance + int(-1 * (list_1[x] - list_2[x]))
+        if(list_1[x] - list_2[x] >= 0):
+            totalDistance = totalDistance + list_1[x] - list_2[x]
+
+    print('Total Distance is:', totalDistance)
+
+
+##This time well do similarity check! How? Well...
+##Check how many time locationID's in the left list appeared in the right list.
+##3 appeared 3 times then 3 * 3 = 9 add to the similarity score.
+##4 appeared 0 times then 4 * 0 = 0 add to the similarity score.
+##Answer is the sum of similarty scores!  
+
+def part_two():
+    sortedList()
+    global totalNum
+    similarity_score = 0; similiar_count = 0 
+        
+    for l in list_1:
+        for r in list_2:
+            if(l == r):
+                similiar_count = similiar_count + 1
+
+        similarity_score = similarity_score + (l * similiar_count)
+        similiar_count = 0     
+
+    print('Similarity Score is: ', similarity_score)
+
+##To get the answer uncomment the function!
+## part_one()
+part_two()
---
+++ 2024_day_2.py
@@ -0,0 +1,13 @@
+##Check if each line is decreasing or increasing
+##Check ifdecreasing or increasing in max steps of 3
+##7 6 4 2 1 safe 'cause its decreasing in 2's
+##1 2 7 8 9 unsafe 'cause it increased 2 -> 7 in 5 steps!
+##1 3 2 4 5 unsafe 'cause it first increased then decreased!
+##Each line is a report so chechk how many reports are safe!
+
+cal_doc = open('2024_day_2_input.txt', 'r')
+
+def part_one():
+    print(4 <= 3)
+
+part_one()
